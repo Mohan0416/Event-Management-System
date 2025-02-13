81ace3f26465a43ccdd5c5d232a155085e1d2b21
Mohan0416 <manomohan417@gmail.com>
2025-02-13 22:15:15 +0530

    login changed

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import AdminDashboard from "../pages/AdminDashboard";
@@ -8,7 +9,8 @@ import UserDashboard from "../pages/UserDashboard";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/admin" element={<AdminDashboard />} />
       <Route path="/user" element={<UserDashboard />} />
